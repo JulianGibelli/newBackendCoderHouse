875881c3e3ee2ee00b5474022ef037e78244984a
JulianGibelli <juligibelli@gmail.com>
2023-03-19 15:48:47 -0300

    queda funcional el deleteProductById del cart

--- src/dao/cartControlador.js
+++ src/dao/cartControlador.js
@@ -15,59 +15,6 @@ export class Cart {
   }
 
   //por medio del id del carrito y el id de producto, se agregara este mismo y su cantidad al carrito
-  /* async addProduct(req, res) {
-    let carrito;
-    let producto;
-    let idCarrito = req.params.cid;
-    console.log("id del carro", idCarrito);
-    let idProducto = req.params.pid;
-    console.log("id del producto", idProducto);
-    let { quantity } = req.body;
-
-    if (!quantity) {
-      res.setHeader("Content-Type", "application/json");
-      return res.status(500).json({
-        mensaje: `Debe incluir cantidad de producto a agregar!`,
-      });
-    }
-
-    try {
-      //voy a buscarlo a mi DB por ese id, si no lo encuentro arroja error
-      carrito = await cartsModelo.find({ _id: idCarrito }).populate("products.product");
-      console.log("soy carrito", carrito);
-    } catch (error) {
-      res.setHeader("Content-Type", "application/json");
-      return res.status(500).json({
-        mensaje: `Error: no se encontro el carrito con ID: ${idCarrito} en la DB`,
-      });
-    }
-
-    //Busco el carrito por id y que tiene el producto a modificar por su id
-    producto = await cartsModelo.find({
-      $and: [{ _id: idCarrito }, { 'products["_id"]': { $eq: `${idProducto}` } }],
-    });
-
-    //Estoy seguro que tengo el carrito pero nose si tiene el producto a modificar/agrgar
-    console.log("soy producto",producto);
-    if (producto.length !== 0) {
-      producto = await cartsModelo.updateOne(
-        {
-          $and: [{ _id: { $eq: `${idCarrito}` } }, { 'products["id"]': { $eq: `${idProducto}` } }],
-        },
-        { $inc: { "products.$.quantity": parseInt(quantity) } }
-      );
-    } else {
-      //si no tengo productos cargados en el carrito actual, es decir, cargaria mi primer elemento!
-      producto = await cartsModelo.updateOne(
-        { _id: { $eq: `${idCarrito}` } },
-        { $set: { products: { id: idProducto, quantity: parseInt(quantity) } } }
-      );
-      res.setHeader("Content-Type", "application/json");
-      res.status(201).json({
-        producto,
-      });
-    }
-  } */
   async addProduct(req, res) {
     res.setHeader("Content-Type", "application/json");
     //toma la cantidad desde el body
@@ -77,7 +24,7 @@ export class Cart {
     //si encontro el carrito
     if (cart) {
       //recorro sus productos
-     
+
       let productIndex = cart.products.findIndex((item) => item._id.toString() == req.params.pid);
       if (productIndex !== -1) {
         await cartsModelo.updateOne(
@@ -107,14 +54,31 @@ export class Cart {
   }
 
   async deleteProduct(req, res) {
+    let carrito;
     let cartId = req.params.cid;
     let productId = req.params.pid;
 
     try {
       //voy a buscarlo a mi DB por ese id, si no lo encuentro arroja error
-      carrito = await cartsModelo.find({ _id: { $eq: `${cartId}` } });
+      carrito = await cartsModelo.findById(cartId);
       console.log(carrito);
-      res.sendStatus(200);
+      if (carrito) {
+        //recorro sus productos
+        let productIndex = carrito.products.findIndex((item) => item._id.toString() == productId);
+        if (productIndex !== -1) {
+          //encontre el producto para eliminar
+          await cartsModelo.updateOne(
+            { _id: req.params.cid },
+            { $pull: { products: { _id:productId } } }
+          );
+        } else {
+          //mandaron otro id y no se encontro
+          return res.status(400).json({ error: "Producto no encontrado" });
+        }
+        return res.status(201).json({ message: "Producto eliminado correctamente..." });
+      } else {
+        return res.status(400).json({ error: "Carrito no encontrado" });
+      }
     } catch (error) {
       res.setHeader("Content-Type", "application/json");
       return res.status(500).json({
